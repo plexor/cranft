db36a1c74421f774a6059d85d8e1033ca30ebb0b
plexor <rcolon85@gmail.com>
2022-03-18 12:27:58 -0400

    Added second button for marketplace

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ const activeChainId = ChainId.Mainnet;
 
 ReactDOM.render(
   <React.StrictMode>
+        <iframe title="This is a unique title" 
+    src="https://gateway.ipfscdn.io/ipfs/QmcH9f3GLFUuGW2REdBVZpgY2ZTTdoZNs9K7QrxsxNDcBV/marketplace.html?contract=0x7E180C3eC7aE1600Ae6e4Dd326E2f92406c5e5b2&chainId=4&listingId=0"
+    width="600px"
+    height="600px"
+    frameborder="0"
+    ></iframe>
+    
+
+  
     <ThirdwebProvider desiredChainId={activeChainId}>
       <App />
     </ThirdwebProvider>
